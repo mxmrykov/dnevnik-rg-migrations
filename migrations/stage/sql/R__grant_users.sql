87e949dde85038afe613b73f3f3f2fe515f3cb25
Maxim Rykov <rykov.maksim2@wb.ru>
2024-08-31 14:12:47 +0300

    Рефакторинг: единые файлы под разные окружения
    Рефакторинг: единые файлы под разные окружения

--- migrations/stage/sql/R__grant_users.sql
+++
@@ -1,3 +0,0 @@
-grant usage on schema users, passwords, classes, notifications, archive, auth to dnevnik_rg_user_stage, dnevnik_rg_user_prod;
-grant execute on all functions in schema users, passwords, classes, notifications, archive, auth to dnevnik_rg_user_stage, dnevnik_rg_user_prod;
-grant insert, select, update, delete on all tables in schema users, passwords, classes, notifications, archive, auth to dnevnik_rg_user_stage, dnevnik_rg_user_prod;
